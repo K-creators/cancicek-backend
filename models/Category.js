20cf3e9865ea64b80f2915d6a862e336865f0b3e
K-creators <karakus.adb@hotmail.com>
2026-01-21 03:12:20 +0300

    Son Güncellemeler!

--- models/Category.js
+++ models/Category.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const CategorySchema = new mongoose.Schema(
   {
     name: { type: String, required: true, unique: true }, // Kategori Adı
+    icon: { type: String, default: "local_florist" },     // İkon ismi
   },
   { timestamps: true }
 );
