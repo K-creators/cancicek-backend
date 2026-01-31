0747fe061fb5b6cd2b66d2350cf6cc83930761a6
K-creators <karakus.adb@hotmail.com>
2026-01-31 20:01:14 +0300

    Hesap Silme özellikleri

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,7 @@ const jwt = require('jsonwebtoken');
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
+const { verifyTokenAndAuthorization } = require("./verifyToken");
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
